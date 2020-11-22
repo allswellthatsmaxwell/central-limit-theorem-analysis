45d35eed3016d24356b412b052cb535d0897f795
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-11-21 20:05:20 -0600

    normalized

--- R/CLTs.R
+++ R/CLTs.R
@@ -8,66 +8,84 @@ library(patchwork)
 ###############################################################################
 ## Plot convolutions  #########################################################
 ###############################################################################
-STEP <- 0.01
-FROM = -2
-TO = 2
+STEP <- 0.001
+FROM = -4
+TO = 4
 steps <- seq(from = FROM, to = TO, by = STEP)
 
 #' Makes a shape along the range FROM to TO, that's 0 everywhere
 #' except for between start and stop. Uses shape_fn to draw.
-make_shape <- function(start, stop, shape_fn) {
-  prop_along <- 1:length(steps) / length(steps)
+make_shape <- function(steps, start, stop, shape_fn) {
   sapply(steps, function(x) {
     if (dplyr::between(x, start, stop)) shape_fn(x) else 0
   })
 }
 
+normalize <- function(x) x / sum(x)
+
 #' Puts f, g, and convolve(f, g) into a long tibble.
 convolve_and_entibble <- function(steps, f, g, name) {
   conv <- convolve(f, g) 
-  ft <- tibble::tibble(name, side = 'f', x = steps, y = f)
-  gt <- tibble::tibble(name, side = 'g', x = steps, y = g)
-  ct <- tibble::tibble(name, side = 'z = f * g', x = steps, 
-                       y = convolve(f, g, type = 'circular'))
+  ft <- tibble::tibble(name, side = ' ', x = steps, y = f)
+  gt <- tibble::tibble(name, side = '*', x = steps, y = g)
+  conv <- convolve(f, g, type = 'circular')
+  ct <- tibble::tibble(name, side = '=', x = steps, 
+                       y = conv)
   dplyr::bind_rows(ft, gt, ct)
 }
+
+post_shaper <- function(x) 4
+f1 <- make_shape(steps, -0.5, 0, post_shaper) %>% normalize()
+g1 <- make_shape(steps, 1, 1.5, post_shaper) %>% normalize()
+name1 <- "uniform * uniform"
+df1 <- convolve_and_entibble(steps, f1, g1, name1)
+
+f2 <- f1 %>% normalize()
+g2 <- convolve(f1, g1) %>% normalize()
+name2 <- "uniform * (uniform * uniform)"
+df2 <- convolve_and_entibble(steps, f2, g2, name2)
+
+gsteps <- seq(from = 0, to = 20, by = STEP * 20)
+f3 <- dgamma(gsteps, 7.5, 1) %>% normalize()
+g3 <- dgamma(gsteps, 2, 2) %>% normalize()
+name3 <- "gamma * gamma"
+df3 <- convolve_and_entibble(gsteps, f3, g3, name3)
+
+bsteps <- seq(from = 0, to = 1, by = STEP)
+f4 <- dbeta(bsteps, 2, 9) %>% normalize()
+g4 <- dbeta(bsteps, 9, 2) %>% normalize()
+name4 <- "beta * beta"
+df4 <- convolve_and_entibble(bsteps, f4, g4, name4)
+
+name5 <- "bimodal * bimodal"
+bisteps <- seq(from = -10, to = 30, by = STEP * 40)
+f5 <- normalize(make_shape(bisteps, 16, 18, post_shaper) + 
+                make_shape(gsteps, 7, 10, post_shaper))
+g5 <- normalize(f4 + g4)
+df5 <- convolve_and_entibble(bisteps, f5, g5, name5)
+
 
 plot_convolutions <- function(dat) {
   dat %>%
     ggplot(aes(x = x, y = y, group = side, color = side)) +
-    facet_wrap(~side, ncol = 1, scales = 'free_y') +
-    scale_color_manual(values = c('f' = 'blue', 'g' = 'red', 'z = f * g' = 'black')) +
-    geom_line() +
+    facet_wrap(~side, ncol = 1, scales = 'free') +
+    scale_color_manual(values = c(' ' = 'blue', '*' = 'red', '=' = 'black')) +
+    geom_line(size = 1.05) +
     theme_bw() +
     theme(panel.grid = element_blank(), 
-          axis.text = element_blank(),
-          axis.ticks = element_blank(),
+          #axis.text.y = element_blank(),
+          #axis.ticks = element_blank(),
           axis.title = element_blank(),
-          legend.position = "none") 
+          legend.position = "none",
+          plot.title = element_text(size = 19, vjust = 0.5),
+          strip.text = element_text(size = 28, vjust = 0.5),
+          strip.background = element_blank())
 }
 
-f1 <- dunif(steps, -1, 0)
-start <- 0
-stop <- 1
-midpoint <- mean(c(start, stop))
-triangle_shaper <- (function() {
-  y <- 0
-  fn <- function(x) {
-    y <<- y + ifelse(x < midpoint, STEP, -STEP)
-    y
-  }
-  fn
-})()
-
-g1 <- make_shape(start, stop, triangle_shaper)
-df1 <- convolve_and_entibble(steps, f1, g1, "A")
-
-post_shaper <- function(x) 4
-f2 <- make_shape(-0.5, 0, post_shaper)
-g2 <- make_shape(1, 1.5, post_shaper)
-df2 <- convolve_and_entibble(steps, f2, g2, "posts")
-
-
-dfs <- list(df1, df2)
+dfs <- list(df1, df2, df3, df4, df5)
 plots <- lapply(dfs, plot_convolutions)
-Reduce(`+`, plots)
+names <- c(name1, name2, name3, name4, name5)
+plots <- Map(function(plot, name) plot + ggtitle(name),
+             plots, names)
+Reduce(`+`, plots) + plot_layout(nrow = 1)
+
