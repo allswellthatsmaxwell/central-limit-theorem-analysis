57e9592f64bfae16f191904dc542a0f867e74e08
Maxwell Peterson <allswellthatsmaxwell9@gmail.com>
2020-11-21 18:46:44 -0600

    two conv functions working

--- R/CLTs.R
+++ R/CLTs.R
@@ -1,7 +1,9 @@
+# install.packages("patchwork") # required
+
 library(dplyr)
 library(magrittr)
 library(ggplot2)
-
+library(patchwork)
 
 ###############################################################################
 ## Plot convolutions  #########################################################
@@ -15,13 +17,8 @@ steps <- seq(from = FROM, to = TO, by = STEP)
 #' except for between start and stop. Uses shape_fn to draw.
 make_shape <- function(start, stop, shape_fn) {
   prop_along <- 1:length(steps) / length(steps)
-  y <- 0
-  draw <- function(x) {
-    y <<- shape_fn(x, y)
-    y
-  }
   sapply(steps, function(x) {
-    if (dplyr::between(x, start, stop)) draw(x) else x
+    if (dplyr::between(x, start, stop)) shape_fn(x) else 0
   })
 }
 
@@ -35,27 +32,42 @@ convolve_and_entibble <- function(steps, f, g, name) {
   dplyr::bind_rows(ft, gt, ct)
 }
 
+plot_convolutions <- function(dat) {
+  dat %>%
+    ggplot(aes(x = x, y = y, group = side, color = side)) +
+    facet_wrap(~side, ncol = 1, scales = 'free_y') +
+    scale_color_manual(values = c('f' = 'blue', 'g' = 'red', 'z = f * g' = 'black')) +
+    geom_line() +
+    theme_bw() +
+    theme(panel.grid = element_blank(), 
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title = element_blank(),
+          legend.position = "none") 
+}
+
 f1 <- dunif(steps, -1, 0)
-start <- 0.4
-stop <- 0.6
+start <- 0
+stop <- 1
 midpoint <- mean(c(start, stop))
-triangle_shaper <- function(x, y) y + ifelse(x < midpoint, STEP, -STEP)
-post_shaper <- function(x, y) 4
+triangle_shaper <- (function() {
+  y <- 0
+  fn <- function(x) {
+    y <<- y + ifelse(x < midpoint, STEP, -STEP)
+    y
+  }
+  fn
+})()
 
 g1 <- make_shape(start, stop, triangle_shaper)
 df1 <- convolve_and_entibble(steps, f1, g1, "A")
 
-f2 <- g2 <- make_shape(start, stop, post_shaper)
+post_shaper <- function(x) 4
+f2 <- make_shape(-0.5, 0, post_shaper)
+g2 <- make_shape(1, 1.5, post_shaper)
 df2 <- convolve_and_entibble(steps, f2, g2, "posts")
 
 
-df1 %>%
-  ggplot(aes(x = x, y = y, group = side, color = side)) +
-  facet_wrap(~side, ncol = 1, scales = 'free_y') +
-  scale_color_manual(values = c('f' = 'blue', 'g' = 'red', 'z = f * g' = 'black')) +
-  geom_line() +
-  theme_bw() +
-  theme(panel.grid = element_blank(), axis.text.y = element_blank(),
-        axis.ticks.y = element_blank()) 
-  
-
+dfs <- list(df1, df2)
+plots <- lapply(dfs, plot_convolutions)
+Reduce(`+`, plots)
